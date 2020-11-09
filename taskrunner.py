7decf5b90717df1372a8b7ec5bad7f07f3ee4498
ymkymkymkymx <43044797+ymkymkymkymx@users.noreply.github.com>
2020-11-08 21:18:28 -0500

    Try to use the weather.py

--- taskrunner.py
+++ taskrunner.py
@@ -1,25 +1,18 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 import time
 sched = BlockingScheduler()
+from weather import *
 
 
 
-
-@sched.scheduled_job('interval', seconds=3)
+@sched.scheduled_job('interval', hours=3)
 def timed_job1():
-    f=open("number.txt","w")
-    num=time.clock()
-    num=str(num)
-    f.write(num)
-    f.close()
-    print('This job is run every three seconds.')
+    thedict=get_current_weather()
+    print('This job is run every three hours.')
 
-@sched.scheduled_job('interval', seconds=3)
+@sched.scheduled_job('interval', hours=3)
 def timed_job2():
-    f=open("number.txt","r")
     
-    num=f.readline()
-    f.close()
-    print('This is the number in number.txt: {0}'.format(num))
+    print('This is the number in number.txt: {0}')
 
 sched.start()
